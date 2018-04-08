44ba30de9293e92528e4ad8251c12e987ca1b014
Farijo <vallarteddy@yahoo.fr>
2018-04-08 18:29:15 +0200

    refactor

--- app/src/main/java/farijo/com/starcraft_bo_shower/network/bo_database/BODatabase.java
+++ app/src/main/java/farijo/com/starcraft_bo_shower/network/bo_database/BODatabase.java
@@ -1,7 +1,5 @@
 package farijo.com.starcraft_bo_shower.network.bo_database;
 
-import android.util.Log;
-
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -15,6 +13,17 @@ import java.sql.SQLException;
 
 public class BODatabase {
 
+    private static final String DEFAULT_IP = "192.168.1.14";
+    private static final String DEFAULT_PORT = "3306";
+    private static final String DEFAULT_DB_NAME = "bo_database_repo";
+    private static final String DEFAULT_DB_USERNAME = "bo_user";
+    private static final String DEFAULT_DB_PASSWORD = "voidRay";
+
+    private String ip = DEFAULT_IP;
+    private String port = DEFAULT_PORT;
+    private String dbName = DEFAULT_DB_NAME;
+    private String dbUsername = DEFAULT_DB_USERNAME;
+    private String dbPassword = DEFAULT_DB_PASSWORD;
     private Connection connection;
 
     public void initConnexion() {
@@ -23,55 +32,67 @@ public class BODatabase {
             public void run() {
                 try {
                     Class.forName("com.mysql.jdbc.Driver");
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                } catch (ClassNotFoundException ignored) {
+
                 }
                 try {
-                    connection = DriverManager.getConnection("jdbc:mysql://192.168.0.13:3306/bo_database_repo", "database_client", null);
-                    synchronized (BODatabase.this) {
-                        BODatabase.this.notifyAll();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                    connection = DriverManager.getConnection("jdbc:mysql://"+ip+":"+port+"/"+dbName, dbUsername, dbPassword);
+                    wakeUpThreadWaitingConnexion();
+                } catch (SQLException ignored) {
+
                 }
             }
         }).start();
+    }
+
+    private synchronized void wakeUpThreadWaitingConnexion() {
+        BODatabase.this.notifyAll();
+    }
+
+    private synchronized void waitConnexion() throws InterruptedException {
+        while (connection == null) {
+            BODatabase.this.wait();
+        }
     }
 
     public void requestBO(final String boName) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (BODatabase.this) {
-                    while (connection == null) {
-                        try {
-                            BODatabase.this.wait();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                try {
+                    waitConnexion();
+                } catch (InterruptedException ignored) {
+
                 }
                 try {
-                    String boRequest =
-                            "SELECT * FROM bo_item " +
-                                    "INNER JOIN bo_item_set ON bo_item.id = bo_item_set.bo_item_id " +
-                                    "INNER JOIN bo ON bo_item_set.id = bo.bo_item_set_id " +
-                                    "WHERE bo.build_name = ?";
-                    final PreparedStatement statement = connection.prepareStatement(boRequest);
-                    statement.setString(1, boName);
-                    ResultSet res = statement.executeQuery();
-                    while (res.next()) {
-                        int id  = res.getInt(1);
-                        int pop = res.getInt(2);
-                        Date time = res.getDate(3);
-                        int entityId = res.getInt(4);
-                        String details = res.getString(5);
-                        Log.d("dfgdgdg", "run: " + id +" | "+ pop +" | "+ time +" | "+ entityId +" | "+ details);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                    PreparedStatement request = prepareBORequestQuery(connection, boName);
+                    ResultSet res = request.executeQuery();
+                    handleBORequestResult(res);
+                } catch (SQLException ignored) {
+
                 }
             }
         }).start();
+    }
+
+    private static PreparedStatement prepareBORequestQuery(Connection connection, String boName) throws SQLException {
+        String boRequest =
+                "SELECT * FROM bo_item " +
+                        "INNER JOIN bo_item_set ON bo_item.id = bo_item_set.bo_item_id " +
+                        "INNER JOIN bo ON bo_item_set.id = bo.bo_item_set_id " +
+                        "WHERE bo.build_name = ?";
+        final PreparedStatement statement = connection.prepareStatement(boRequest);
+        statement.setString(1, boName);
+        return statement;
+    }
+
+    private static void handleBORequestResult(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            int id  = resultSet.getInt(1);
+            int pop = resultSet.getInt(2);
+            Date time = resultSet.getDate(3);
+            int entityId = resultSet.getInt(4);
+            String details = resultSet.getString(5);
+        }
     }
 }
